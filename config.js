b7dc0243cfd773c202c1003d15633611fd7d360a
YANZGANZ <102246383+YANZGANZ@users.noreply.github.com>
2022-03-27 11:37:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['6281227144597', 'ozan', true],
+  ['48729759866', 'ozan', true],
   //['6281227144597']
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
